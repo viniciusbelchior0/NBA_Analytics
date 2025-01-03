541f3420f7a966ff0fff57da7d146d9bebf78b1d
Vinicius Belchior <89149399+viniciusbelchior0@users.noreply.github.com>
2025-01-03 17:45:12 -0300

    Add files via upload

---
+++ scripts/orquestracao_airflow_nba.py
@@ -0,0 +1,16 @@
+from airflow import DAG 
+from airflow.operators.bash_operator import BashOperator
+import pendulum
+
+with DAG(
+    "nba_stats",
+    start_date=pendulum.datetime(2024, 5, 1, tz="UTC"),
+    schedule_interval='0 12 * * *', # executar todos os dias ao meio-dia
+) as dag:
+
+    nba_stats = BashOperator(
+        task_id = 'nba_stats',
+        bash_command = 'Rscript C:/Users/NOTEBOOK CASA/Desktop/NBA_Analytics/nba_full.R'
+    )
+
+    nba_stats 
